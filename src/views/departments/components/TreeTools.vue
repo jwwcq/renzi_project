c0e61cef2dd65ef3942c6cc645d26786cdb2053e
jwwcq <1461206455@qq.com>
2022-09-30 13:46:49 +0800

    添加子部门完成

---
+++ src/views/departments/components/TreeTools.vue
@@ -0,0 +1,59 @@
+<template>
+
+  <el-row type="flex" justify="space-around" align="middle" style="height:40px;width: 100%;">
+    <el-col>
+      <span>{{ treeNode.name }}</span>
+    </el-col>
+    <el-col :span="4">
+      <el-row type="flex">
+        <el-col>
+          <span style="margin-right: 8px;">{{ treeNode.manager }}</span>
+        </el-col>
+        <el-col>
+          <el-dropdown @command="handleCommand">
+            <span>
+              操作<i class="el-icon-arrow-down el-icon--right" />
+            </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item command="add">添加子部门</el-dropdown-item>
+              <el-dropdown-item v-if="isRoot" command="edit">编辑部门</el-dropdown-item>
+              <el-dropdown-item v-if="isRoot" command="del">删除部门</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-col>
+      </el-row>
+    </el-col>
+  </el-row>
+
+</template>
+
+<script>
+export default {
+  name: 'HrsaasTreeTools',
+  props: {
+    treeNode: {
+      type: Object,
+      required: true
+    },
+    isRoot: {
+      type: Boolean,
+      default: true
+    }
+  },
+  methods: {
+    handleCommand(type) {
+      if (type === 'add') {
+        this.$emit('addDept', this.treeNode)
+      } else if (type === 'edit') {
+        22
+      } else {
+        33
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,16 +1,64 @@
 <template>
-  <div>
-    depa
-
+  <div class="departments-container">
+    <el-card>
+      <tree-tools :tree-node="company" :is-root="false" @addDept="addDepts" />
+    </el-card>
+    <el-tree :data="data" :default-expand-all="true" :props="defaultProps">
+      <tree-tools slot-scope="{data}" :tree-node="data" @addDept="addDepts" />
+    </el-tree>
+    <add-dept :dialog-visible.sync="dialogVisible" :tree-node="currentNode" />
   </div>
 </template>
 
 <script>
+import TreeTools from '@/views/departments/components/TreeTools'
+import { getDepartmentsAPI } from '@/api/departments'
+import { tranListToTreeData } from '@/utils'
+import AddDept from '@/views/departments/components/AddDept'
 export default {
-
+  components: {
+    TreeTools,
+    AddDept
+  },
+  data() {
+    return {
+      data: [{ name: '总裁办', manager: '曹操', children: [{ name: '董事会', manager: '曹丕' }] },
+        { name: '行政部', manager: '刘备' },
+        { name: '人事部', manager: '孙权' }],
+      defaultProps: {
+        label: 'name'
+      },
+      company: {},
+      depts: [],
+      dialogVisible: false,
+      currentNode: {}
+    }
+  },
+  mounted() {
+    this.getDepartments()
+  },
+  methods: {
+    async  getDepartments() {
+      const { depts, companyName, companyManage } = await getDepartmentsAPI()
+      this.data = tranListToTreeData(depts, '')
+      this.company = { name: companyName, manager: companyManage, id: '' }
+    },
+    addDepts(node) {
+      this.dialogVisible = true
+      this.currentNode = node
+    }
+  }
 }
 </script>
 
-<style>
-
-</style>
+<style lang="scss">
+  .departments-container {
+    width: 900px;
+    margin: 20px auto;
+    .el-tree {
+      .el-tree-node__content {
+        padding-right: 20px;
+      }
+    }
+  }
+  </style>
